7d7669bec6f249ccba1eb76190cc20623e80a4af
magix257 <55533437+magix257@users.noreply.github.com>
2019-11-18 13:37:08 +0100

    Dziala Id generowane zapis do bazy bez podania id oraz odczyt wszystkich i odczyt 1 po zapisaniu

--- src/main/java/pl/slawek/HomeController.java
+++ src/main/java/pl/slawek/HomeController.java
@@ -3,6 +3,7 @@ package pl.slawek;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -24,9 +25,18 @@ public class HomeController {
 	public String addZlecenie(@ModelAttribute Zlecenia z, Model m) 
 	{
 		repo.save(z);
+		m.addAttribute("result", repo.findById(z.getId()));
 		
 		return "result.jsp";
 	}
 	
+	@GetMapping("getZlecenia")
+	public String getStudents(@ModelAttribute Zlecenia z, Model m)
+	{
+		
+		m.addAttribute("result", repo.findAll());
+		
+		return "result.jsp";
+	}
 		
 }
